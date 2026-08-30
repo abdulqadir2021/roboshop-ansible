572427d78686a3c7ab5c0ee7502e411328b5d8dc
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2026-08-30 18:20:41 +0530

    project roboshop 26

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 git pull
+
 if [ $# -ne 2 ]; then
   echo "Input Missing"
   echo "Usage: bash run.sh <component-name> <env>"
   exit 1
 fi
-ansible-playbook -i $1-$2.yeloabhi.fun, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e app_name=$1 roboshop.yml
 
+ansible-playbook -i "$1-$2.yeloabhi.fun," \
+  -e ansible_user=ec2-user \
+  -e ansible_password=DevOps321 \
+  -e env="$2" \
+  -e app_name="$1" \
+  roboshop.yml
